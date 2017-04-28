c9a02bc6baa234be98f3d9b422729cb636e362b0
shirleyjwa <youngshirley98@gmail.com>
2017-04-27 22:12:24 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,6 +1,52 @@
-public class Player {
-
-  public Player(){
-  }
-
+public class Player{
+    //Fields
+    protected String name;
+    protected int health;
+    protected int minDamage;
+    protected int maxDamage;
+    //Constructor
+    public Player(String name, int health, int minDamage, int maxDamage){
+        this.name= name;
+        this.health= health;
+        this.minDamage= minDamage;
+        this.maxDamage= maxDamage;
+    }
+    //Accessors
+    public String getName(String name){
+        return name;
+    }
+    public int getHealth(){ 
+        return health;
+    }
+    public int getMinDamage(){
+        return minDamage;
+    }
+    public int getMaxDamage(){
+        return maxDamage;
+    }
+    //Mutators
+    public void setName(String name){
+        this.name= name;
+    }
+    public void setHealth(int health){
+        this.health= health;
+    }
+    public void setMinDamage(int minDamage){
+        this.minDamage= minDamage;
+    }
+    public void setMaxDamage(int maxDamage){
+        this.maxDamage= maxDamage;
+    }
+    //Methods
+    public void receiveDamage(int damage){
+        if (health>=damage){
+            health= health- damage;
+            System.out.println(name+" has "+health+" health left.");
+        }
+    }
+    public void attack(Player enemy){
+        int randomDamage= (int)(Math.random()*(maxDamage-minDamage+1)+ minDamage); //random amount of damage
+        enemy.receiveDamage(randomDamage);
+        System.out.println(name+" attacks the "+enemy+" doing "+randomDamage+" damage.");
+    }
 }
